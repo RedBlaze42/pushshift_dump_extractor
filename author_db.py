69a666e19ab1b75788c25612970bc2fe46e2951b
RedBlaze42 <allairehugo@gmail.com>
2021-08-23 18:02:16 +0200

    Fix

--- author_db.py
+++ author_db.py
@@ -4,16 +4,19 @@ class AuthorDb:
 
     def __init__(self, db_path = "authors.db"):
         self.file_path  = db_path
-        self.db = sqlite3.connect(self.file_path)   
+        self.db = sqlite3.connect(self.file_path)
+        
+        self.db.execute("PRAGMA synchronous = OFF")
+        self.db.execute("PRAGMA journal_mode = OFF")
 
-        self.db.execute("CREATE TABLE IF NOT EXISTS author_posts (id INTEGER PRIMARY KEY AUTOINCREMENT, author_name TEXT, subreddit INTEGER, posts INTEGER);")
+        self.db.execute("CREATE TABLE IF NOT EXISTS author_posts (id INTEGER PRIMARY KEY AUTOINCREMENT, author_name TEXT NOT NULL, subreddit INTEGER NOT NULL, posts INTEGER);")
 
     def close(self):
         self.db.commit()
         self.db.close()
 
     def add_post(self, name, subreddit):
-        self.db.execute("INSERT INTO author_posts (author_name, subreddit, posts) VALUES (?, ?, 1) ON DUPLICATE KEY UPDATE posts = posts + 1;", (name, subreddit))
+        self.db.execute("INSERT INTO author_posts (author_name, subreddit, posts) VALUES (?, ?, 1) ON CONFLICT(author_name, subreddit) DO UPDATE SET posts = posts + 1;", (name, subreddit))
     
     def get_authors(self):
         cursor = self.db.cursor()
