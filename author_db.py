8fe6a79c7f67c5dc765edd9f9987dee951607c46
RedBlaze42 <allairehugo@gmail.com>
2021-08-23 17:30:37 +0200

    commit db before closing

--- author_db.py
+++ author_db.py
@@ -5,9 +5,12 @@ class AuthorDb:
     def __init__(self, db_path = "authors.db"):
         self.file_path  = db_path
         self.db = sqlite3.connect(self.file_path)   
-        self.close = self.db.close
 
         self.db.execute("CREATE TABLE IF NOT EXISTS author_posts (id INTEGER PRIMARY KEY AUTOINCREMENT, author_name TEXT, subreddit INTEGER, posts INTEGER);")
+
+    def close(self):
+        self.db.commit()
+        self.db.close()
 
     def add_post(self, name, subreddit):
         self.db.execute("INSERT INTO author_posts (author_name, subreddit, posts) VALUES (?, ?, 1) ON DUPLICATE KEY UPDATE posts = posts + 1;", (name, subreddit))
